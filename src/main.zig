d5ea6ee1ee28b25e1b289ab64b66a71963289392
Pedro Cattori <pcattori@gmail.com>
2025-12-29 12:42:43 -0500

    cli

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello, world!\n", .{});
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    switch (args.len) {
+        0 => unreachable,
+        1 => std.debug.print("repl\n", .{}),
+        2 => std.debug.print("script: {s}\n", .{args[1]}),
+        else => {
+            std.debug.print("Usage: zlox [script]\n", .{});
+            std.process.exit(64);
+        },
+    }
 }
