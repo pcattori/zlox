77f010618ec3854be20a962638df657827c72c08
Pedro Cattori <pcattori@gmail.com>
2025-12-30 18:06:30 -0500

    repl: unbuffered stdout

--- src/repl.zig
+++ src/repl.zig
@@ -5,16 +5,13 @@ pub fn repl() !void {
     var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
     const stdin = &stdin_reader.interface;
 
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    var stdout_writer = std.fs.File.stdout().writer(&.{});
     const stdout = &stdout_writer.interface;
 
     while (true) {
         try stdout.writeAll("zlox> ");
-        try stdout.flush();
         const line = try stdin.takeDelimiter('\n') orelse continue;
         if (line.len == 0) continue;
         try stdout.print("echo: {s}\n", .{line});
-        try stdout.flush();
     }
 }
