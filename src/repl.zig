3c876456279725b50c6adf5ff047d6fc5c0709a6
Pedro Cattori <pcattori@gmail.com>
2025-12-30 18:15:04 -0500

    repl: use io interfaces directly

--- src/repl.zig
+++ src/repl.zig
@@ -2,16 +2,14 @@ const std = @import("std");
 
 pub fn repl() !void {
     var stdin_buffer: [1024]u8 = undefined;
-    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
-    const stdin = &stdin_reader.interface;
+    var stdin = std.fs.File.stdin().reader(&stdin_buffer);
 
-    var stdout_writer = std.fs.File.stdout().writer(&.{});
-    const stdout = &stdout_writer.interface;
+    var stdout = std.fs.File.stdout().writer(&.{});
 
     while (true) {
-        try stdout.writeAll("zlox> ");
-        const line = try stdin.takeDelimiter('\n') orelse continue;
+        try stdout.interface.writeAll("zlox> ");
+        const line = try stdin.interface.takeDelimiter('\n') orelse continue;
         if (line.len == 0) continue;
-        try stdout.print("echo: {s}\n", .{line});
+        try stdout.interface.print("echo: {s}\n", .{line});
     }
 }
