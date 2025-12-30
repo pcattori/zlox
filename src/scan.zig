ac26a0405b2728d2ff03abaa08afd4b0b26855d2
Pedro Cattori <pcattori@gmail.com>
2025-12-29 21:37:47 -0500

    token module

--- src/scan.zig
+++ src/scan.zig
@@ -1,57 +1,7 @@
 const std = @import("std");
-const Span = @import("span.zig").Span;
+
 const Diagnostics = @import("./diagnostics.zig").Diagnostics;
-
-const Token = struct {
-    kind: Kind,
-    span: Span,
-
-    const Kind = enum {
-        left_paren,
-        right_paren,
-        left_brace,
-        right_brace,
-        comma,
-        dot,
-        minus,
-        plus,
-        semicolon,
-        star,
-
-        bang,
-        bang_equal,
-        equal,
-        equal_equal,
-        greater,
-        greater_equal,
-        less,
-        less_equal,
-        slash,
-
-        string,
-        number,
-        identifier,
-
-        kw_and,
-        kw_class,
-        kw_else,
-        kw_false,
-        kw_for,
-        kw_fun,
-        kw_if,
-        kw_nil,
-        kw_or,
-        kw_print,
-        kw_return,
-        kw_super,
-        kw_this,
-        kw_true,
-        kw_var,
-        kw_while,
-
-        eof,
-    };
-};
+const Token = @import("token.zig").Token;
 
 const keywords = [_]struct {
     name: []const u8,
